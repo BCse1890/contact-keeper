4e479dc26314e63de8281685418d238990b66b88
BCse1890 <bob.currie@fsresearch.co.uk>
2019-07-25 23:13:35 +0100

    Add ContactForm, finalise ContactState and ContactReducer elements

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -8,12 +8,20 @@ const Contacts = () => {
   // can access anything in Context using above, so can access contacts
   // array from ContactState
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please Add a Contact: </h4>;
+  }
   return (
     <Fragment>
-      {contacts.map(contact => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
+      {filtered !== null
+        ? filtered.map(contact => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))
+        : contacts.map(contact => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))}
     </Fragment>
   );
 };
