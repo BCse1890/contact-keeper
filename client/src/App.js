0b67e54efd940a1fe1c1fd588b4555dcbb35f437
BCse1890 <bob.currie@fsresearch.co.uk>
2019-07-26 16:18:39 +0100

    Add Auth context and state, Alert context and State

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,39 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
+import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-import Navbar from "./components/layout/Navbar";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import ContactState from "./context/contact/ContactState";
-import Contacts from "./components/contacts/Contacts";
+import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alerts/AlertState";
+import Alert from "./components/layout/Alert";
+import "./App.css";
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-          <h1>My App</h1>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alert />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+              <h1>My App</h1>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
 };
 
